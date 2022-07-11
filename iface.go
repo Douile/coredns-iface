3a15b72d115f72814b53b385ea691272298024d3
Douile <25043847+Douile@users.noreply.github.com>
2022-07-11 23:28:21 +0100

    Fix code

--- iface.go
+++ iface.go
@@ -5,54 +5,56 @@ import (
 	"net"
 	"strings"
 
-	"github.com/coredns/coredns/request"
 	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/request"
 
 	"github.com/miekg/dns"
 )
 
-type IFace struct{}
+type IFace struct {
+	Next plugin.Handler
+}
 
 // ServeDNS implements the plugin.Handler interface.
 func (p IFace) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	c := request.Request{W: w, Req: r}
-	qname := c.Name()
+	state := request.Request{W: w, Req: r}
+	qname := state.Name()
 
-  if !strings.HasSuffix(qname, ".iface.") || (c.QType != dns.TypeA && c.QType != dns.TypeAAAA) {
-    return plugin.NextOrFailure(c.Name(), c.Next, ctx, w, r)
-  }
+	if !strings.HasSuffix(qname, ".iface.") || (state.QType() != dns.TypeA && state.QType() != dns.TypeAAAA) {
+		return plugin.NextOrFailure(p.Name(), p.Next, ctx, w, r)
+	}
 
-  ifaceName := strings.TrimSuffix(qname, ".iface.")
+	ifaceName := strings.TrimSuffix(qname, ".iface.")
 
-  iface, err := net.InterfaceByName(ifaceName)
+	iface, err := net.InterfaceByName(ifaceName)
 
-  if err != nil {
-    return dns.RcodeNameError, nil
-  }
+	if err != nil {
+		return dns.RcodeNameError, nil
+	}
 
-  addrs, err := iface.Addrs()
+	addrs, err := iface.Addrs()
 
-  if err != nil {
-    return dns.RcodeNameError, nil
-  }
+	if err != nil {
+		return dns.RcodeNameError, nil
+	}
 
 	answers := []dns.RR{}
-  for _, addr := range addrs {
-    ip := net.ParseIP(addr.String())
-    if ip.To4() != nil {
-      rr := new(dns.A)
-      rr.Hdr = dns.RR_Header{Name: qname, Rrtype: dns.TypeA, Class: dns.ClassINET}
-      rr.A = ip.To4()
+	for _, addr := range addrs {
+		ip := net.ParseIP(addr.String())
+		if ip.To4() != nil {
+			rr := new(dns.A)
+			rr.Hdr = dns.RR_Header{Name: qname, Rrtype: dns.TypeA, Class: dns.ClassINET}
+			rr.A = ip.To4()
 
-      answers = append(answers, rr)
-    } else if ip.To16() != nil {
-      rr := new(dns.AAAA)
-      rr.Hdr = dns.RR_Header{Name: qname, Rrtype: dns.TypeAAAA, Class: dns.ClassINET}
-      rr.AAAA = ip.To16()
+			answers = append(answers, rr)
+		} else if ip.To16() != nil {
+			rr := new(dns.AAAA)
+			rr.Hdr = dns.RR_Header{Name: qname, Rrtype: dns.TypeAAAA, Class: dns.ClassINET}
+			rr.AAAA = ip.To16()
 
-      answers = append(answers, rr)
-    }
-  }
+			answers = append(answers, rr)
+		}
+	}
 
 	m := new(dns.Msg)
 	m.SetReply(r)
